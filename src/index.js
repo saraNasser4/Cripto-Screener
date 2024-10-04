942c92cd6207b3e0505880139c712fb2cc8c46dd
saraNasser4 <saransser6@gmail.com>
2024-10-04 13:55:49 +0300

    adding react router dom

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from './pages/Home'
+import Crypto from './pages/Crypto'
+import Trending from './pages/Trending'
+import Saved from './pages/Saved'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <Crypto />
+      },
+      {
+        path: "/trending",
+        element: <Trending />
+      },
+      {
+        path: "/saved",
+        element: <Saved />
+      },
+    ]
+  } 
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
